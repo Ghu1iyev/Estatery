9cf00a8b3f3b264423196e76ca40983563998f51
Ghu1iyev <tu12n4c1r@code.edu.az>
2023-09-12 16:06:05 +0400

    updated

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
     <PropertyProvider>
     <App />
-    <ReactQueryDevtools initialIsOpen={false}/>
     </PropertyProvider>
     </QueryClientProvider>
   </React.StrictMode>,
